a2433f8f74359c1fb9c397b5d5c3cb71d9faf8d2
Tom Davies <tom@tomdavies.net>
2013-06-13 17:22:38 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,39 +1,90 @@
-ed.jquery.plugin.template.js
+ed.jquery.toggler.js
 ============================
 
-A template for creating a jQuery plugin that can optionally be loaded as an AMD module
+ A simple jQuery Plugin for toggling the visibility of elements, that can optionally be loaded as an AMD module.
 
+## Why?
+
+Because show me a website that uses jQuery and doesn't show/hide dom elements.
 
 ## Dependencies
 
-* jQuery
+* A vaguely recent version of jQuery, 1.9 is probably not a bad idea, but 1.7+ _should_ be fine (untested).
 
 ## Installation
 
-    bower install ed.jquery.plugin.template
+    bower install ed.jquery.toggler
 
 ## Usage
 
-Invoke directly:
+`toggler()` binds to an arbitrary toggle element and looks at either it's `href` attribute (in the first instance) or for a `data-target` attribute, which should contain an id value (including the `#`) for a target in the same document. It then shows or hides that element based on the target's current visibility, using the options you set. You set the initial visibility of your targets with CSS.
 
-    $('.my-class').pluginName();
+It works with `click` events on anchors and `<button>` elements and with `change` events on form `<input>`/`<select>` elements.
 
-Or auto initialise on click with data attributes:
 
-    <span data-plugin-name="badgerify">I like badgers</span>
+Thats about it.
+### Binding to all the things:
+
+Invoke directly on a collection:
+
+    $('.my-class').toggler();
+
+Or auto initialise on click/change via the magic of `data` attributes and event delegation:
+
+    <button data-target="#my-id" data-toggle>Toggle content</button>
+    <a href="#my-id" data-toggle>Toggle content</a>
+    <div id="#my-id">
+        Content to toggle.
+    </div>
 
 ## Configuration
 
+Set options by passing a config option when bound:
+
+    $('.my-class').toggler({
+        methodIn: 'fadeIn',
+        speedIn: 500
+    });
+
+Or set global defaults via the `$.fn.toggler.defaults` object, before your toggler instance(s) has been created:
+
+    $.fn.toggler.defaults.methodIn = 'fadeIn';
+
+## Options and defaults
+
+* `methodIn`:   jQuery method to use when showing the target, as a string
+* `speedIn`:    speed in milliseconds to use when showing the target
+* `inCallback`: callback fn to be executed after target shown
+* `methodOut`:  jQuery method to use when hiding the target, as a string
+* `speedOut`:   speed in milliseconds to use when hiding the target
+* `outCallback`: callback fn to be executed after target shown
+* `targetInClass`: HTML classname applied to the target after it has finished showing. Only applied after the target has been shown by the plugin for the first time
+* `targetOutClass`: HTML classname applied to the target after it has finished hiding. Only applied after the target has been hidden by the plugin for the first time
+* `targetAnimatingClass`: HTML classname applied to the target while it is animating.
+* `triggerTargetInClass`: HTML classname applied to the *trigger* element when its target is visible. Only applied after the target has been shown or hidden for the first time
+
 ### Defaults
+
+
+    $.fn.toggler.defaults = {
+        methodIn: 'slideDown',
+        speedIn: 200,
+        methodOut: 'slideUp',
+        speedOut: 200,
+        targetInClass: 'toggler--in',
+        targetOutClass: 'toggler--out',
+        targetAnimatingClass: 'toggler--animating',
+        triggerTargetInClass: 'toggler--target-in'
+    };
+
 
 ## To do
 
-* Unit test templates
+* Unit tests
 
-## Credits
+## Bugs / contributing
 
-* Universal Module Definition pattern: [UMD.js](https://github.com/umdjs/umd)
-* Data Attribute auto-initialisation pattern, [Twitter Bootstrap](https://github.com/twitter/bootstrap)
+The former are likely, the latter welcome. Please open an issue / pull request as appropriate.
 
 ## Licence
 
---
+++ jquery.toggler.js
@@ -0,0 +1,141 @@
+/* global define, jQuery */
+/**
+ * A simple jQuery Plugin for toggling the visibility of elements
+ * 
+ * @author  Tom Davies - Erskine Design
+ * @version  0.5.0
+ */
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+
+    var Toggler = function(el, opts) {
+        var $trigger = this.$trigger = $(el);
+        this.opts = opts;
+
+        this.targetSel = $trigger.attr('href') || $trigger.attr('data-target');
+        this.$target = $(this.targetSel);
+
+        this.init();
+    };
+
+    Toggler.prototype = {
+        constructor: Toggler,
+        init: function() {
+            this.addListeners();
+        },
+
+        addListeners: function() {
+            var _self = this;
+            this.$trigger.off('change.toggler click.toggler');
+            this.$trigger.on('change.toggler', function(e){
+                _self.toggle();
+            });
+            if (this.$trigger.is('a, button')){
+                this.$trigger.on('click.toggler', function(e){
+                    e.preventDefault();
+                    _self.toggle();
+                });
+            }
+        },
+
+        toggle: function(e) {
+            return (this.targetIsVisible() ? this.hide() : this.show());
+        },
+        show: function(){
+            var method = this.opts.methodIn,
+                speed  = this.opts.speedIn,
+                callback = this.opts.inCallback,
+                inClass = this.opts.targetInClass,
+                outClass = this.opts.targetOutClass;
+
+            this.__transition(method, speed, callback, inClass, outClass);
+        },
+        hide: function(){
+            var method = this.opts.methodOut,
+                speed  = this.opts.speedOut,
+                callback = this.opts.outCallback,
+                inClass = this.opts.targetInClass,
+                outClass = this.opts.targetOutClass;
+
+            this.__transition(method, speed, callback, outClass, inClass);
+        },
+        __transition: function(method, speed, callback, classToAdd, classToRemove){
+            var _self = this,
+                animClass = this.opts.targetAnimatingClass,
+                triggerClass = this.opts.triggerTargetInClass;
+
+            this.$target.removeClass(classToRemove).addClass(animClass);
+
+            this.$target.stop()[method](speed, function(){
+                var triggerMethod;
+
+                //stopped animating, now in or out
+                _self.$target.removeClass(animClass).addClass(classToAdd);
+
+                //toggle the class on the trigger
+                triggerMethod = _self.targetIsVisible ? 'addClass' : 'removeClass';
+                _self.$trigger[triggerMethod](triggerClass);
+
+                if ($.isFunction(callback)){
+                    callback();
+                }
+
+            });
+        },
+        targetIsVisible: function(){
+            return this.$target.is(':visible');
+        }
+    };
+
+    $.fn.toggler = function(option) {
+        var opts;
+
+        if (typeof option === 'object' && option) {
+            opts = $.extend(true, {}, $.fn.toggler.defaults, option);
+        } else {
+            opts = $.fn.toggler.defaults;
+        }
+        return this.each(function() {
+            var $this = $(this),
+            // don't call again if already initialised on this object
+            data = $this.data('toggler');
+            if(!data){
+                $this.data('toggler', data = new Toggler(this, opts));
+            }
+            // allow the calling of plugin methods on an instance by name, eg: $item.toggler('show')
+            if (typeof option === 'string') {
+                data[option]();
+            }
+        });
+    };
+
+    $.fn.toggler.defaults = {
+        methodIn: 'slideDown',
+        speedIn: 200,
+        methodOut: 'slideUp',
+        speedOut: 200,
+        targetInClass: 'toggler--in',
+        targetOutClass: 'toggler--out',
+        targetAnimatingClass: 'toggler--animating',
+        triggerTargetInClass: 'toggler--target-in'
+    };
+    $(document).on('change.toggler', '[data-toggle]', function(){
+        if(!$(this).data('toggler')){
+            $(this).toggler('toggle');
+        }
+    });
+
+    $(document).on('click.toggler', 'a[data-toggle]', function(e){
+        if(!$(this).data('toggler')){
+            e.preventDefault();
+            $(this).toggler('toggle');
+        }
+    });
+}));
